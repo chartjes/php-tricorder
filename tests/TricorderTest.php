6db75b94caf187005f9a6b5d225689a478bcb857
Chris Hartjes <chartjes@littlehart.net>
2013-04-18 21:59:08 -0400

    Cleaned up the test to use a data provider

--- tests/TricorderTest.php
+++ tests/TricorderTest.php
@@ -38,31 +38,49 @@ class TricorderTest extends \PHPUnit_Framework_TestCase
         $this->assertContains('Scanning ReferenceClass', self::$output);
     }
 
+    /**
+     * Data provider for verifying that specific suggestions appear
+     * in the output
+     */
+    public function suggestionsDataProvider()
+    {
+        return array(
+            array('returnBoolean -- test method returns boolean values'),
+            array('acceptStringParam -- test $value using null or empty strings'),
+            array('returnVoid -- test method returns void instances'),
+            array('acceptIntegerParam -- test $foo using non-integer values'),
+            array('returnArray -- test method returns array instances'),
+            array('acceptGrumpyFoo -- mock $foo as \Grumpy\Foo'),
+            array('returnInteger -- test method returns non-integer values'),
+            array('acceptFloatParam -- mock $value as float'),
+            array('./TestClass.php -- \Grumpy\Dependency\Foo might need to be injected for testing purposes'),
+            array('./TestClass.php -- \Grumpy\Foo might need to be injected for testing purposes due to static method call'),
+        array('returnSpecificObjectType -- test method returns \Grumpy\Foo instances'), array('_protectedMethod -- non-public methods are difficult to test in isolation'),
+        );
+    }
+
+    /**
+     * Make sure that specific suggestions are generated based on our reference
+     * class
+     *
+     * @test
+     * @dataProvider suggestionsDataProvider
+     */
+    public function checkForSpecificSuggestions($suggestion)
+    {
+        $this->assertContains($suggestion, self::$output);
+    } 
+
     public function testShouldSuggestToTestTheProtectedAttributeForAnObjectType()
     {
         $this->markTestIncomplete('todo');
-    }
-
-    public function testShouldSuggestToTestForReturnedBooleanValue()
-    {
-        $this->assertContains('returnBoolean -- test method returns boolean values', self::$output);
     }
 
     public function testShouldNotSuggestCoveredAnnotationOfBoolean()
     {
         $this->assertNotContains('returnBooleanCovered', self::$output);
     }
-
-    public function testShouldSuggestToTestStringArgumentWithNullOrEmptyString()
-    {
-        $this->assertContains('acceptStringParam -- test $value using null or empty strings', self::$output);
-    }
-
-    public function testShouldSuggestToTestForVoidReturnedValue()
-    {
-        $this->assertContains('returnVoid -- test method returns void instances', self::$output);
-    }
-
+    
     public function testShouldNotOutputCoveredAnnotationForStringArgumentValue()
     {
         $this->assertNotContains('acceptStringParamCovered', self::$output);
@@ -72,17 +90,7 @@ class TricorderTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertNotContains('returnVoidCovered', self::$output);
     }
-
-    public function testShouldSuggestToTestIntegerArgumentWithNonIntegerValue()
-    {
-        $this->assertContains('acceptIntegerParam -- test $foo using non-integer values', self::$output);
-    }
-
-    public function testShouldSuggestToTestReturnedArray()
-    {
-        $this->assertContains('returnArray -- test method returns array instances', self::$output);
-    }
-
+    
     public function testShouldNotOutputCoveredMethodAcceptsIntegerValuesAndArrayValues()
     {
         $this->assertNotContains('acceptIntegerParamCovered', self::$output);
@@ -93,21 +101,7 @@ class TricorderTest extends \PHPUnit_Framework_TestCase
         $this->assertNotContains('returnArrayCovered', self::$output);
     }
 
-    public function testShouldSuggestToTestObjectArgumentUsingMock()
-    {
-        $this->assertContains('acceptGrumpyFoo -- mock $foo as \Grumpy\Foo', self::$output);
-    }
 
-    public function testShouldSuggestToTestReturnedIntegerValue()
-    {
-        $this->assertContains('returnInteger -- test method returns non-integer values', self::$output);
-    }
-
-    public function testShouldSuggestToTestFloatArgument()
-    {
-        // @todo change output of message (Mock $value for float)
-        $this->assertContains('acceptFloatParam -- mock $value as float', self::$output);
-    }
 
     public function testShouldSuggestToTestReturnedValueFromMockObject()
     {
@@ -119,26 +113,6 @@ class TricorderTest extends \PHPUnit_Framework_TestCase
         $this->markTestIncomplete('passArgumentToObject: Suggest to test argument passed to mock ->with($value)');
     }
 
-    public function testShouldWarnAboutUsingDependencyInMethod()
-    {
-        $this->assertContains('./TestClass.php -- \Grumpy\Dependency\Foo might need to be injected for testing purposes', self::$output);
-    }
 
-    public function testShouldWarnAboutUsingStaticCallInMethods()
-    {
-        $this->assertContains(
-            './TestClass.php -- \Grumpy\Foo might need to be injected for testing purposes due to static method call',
-            self::$output
-        );
-    }
 
-    public function testShouldSuggestToTestReturnInstanceType()
-    {
-        $this->assertContains('returnSpecificObjectType -- test method returns \Grumpy\Foo instances', self::$output);
-    }
-
-    public function testShouldWarnAboutUsingProtectedMethods()
-    {
-        $this->assertContains('_protectedMethod -- non-public methods are difficult to test in isolation', self::$output);
-    }
 }
